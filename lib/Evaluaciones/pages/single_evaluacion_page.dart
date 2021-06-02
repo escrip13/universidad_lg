541b1161e38c46ce43773f475ce5ae8e401626f2
escrip13 <escrp13@gmail.com>
2021-06-02 17:05:13 -0500

    cambio de preguntas

--- lib/Evaluaciones/pages/single_evaluacion_page.dart
+++ lib/Evaluaciones/pages/single_evaluacion_page.dart
@@ -11,6 +11,7 @@ import 'package:universidad_lg/User/pages/login_page.dart';
 import 'package:universidad_lg/widgets/drawer_menu_left.dart';
 import 'package:universidad_lg/widgets/drawer_menu_right.dart';
 import 'package:cool_stepper/cool_stepper.dart';
+import 'package:radio_button_form_field/radio_button_form_field.dart';
 
 import '../../constants.dart';
 
@@ -144,30 +145,72 @@ class _ContentSingleEvaluacion extends StatefulWidget {
 }
 
 class __ContentSingleEvaluacionState extends State<_ContentSingleEvaluacion> {
-  String selectedRole = '';
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
   Map preguntasList = {};
-
-  void _list() => print(preguntasList);
-
-  _setValue(pre, res) {
-    setState(() {
-      preguntasList[pre] = res;
-    });
-  }
+  List<CoolStep> steps = [];
+  bool cargo = false;
+  String selectedRole = '';
+  bool _autoValidate = false;
 
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      if (!cargo) {
+        listSteps(context);
+        cargo = true;
+      }
+    });
+
     // startTimeout();
     // print(totalTime);
 
-    _list();
+    // print(preguntasList);
 
-    final List<CoolStep> steps = [];
+    return Container(
+      child: Form(
+        key: _key,
+        autovalidateMode:
+            _autoValidate ? AutovalidateMode.always : AutovalidateMode.disabled,
+        child: Stack(children: [
+          CoolStepper(
+            showErrorSnackbar: false,
+            onCompleted: () {
+              _onSave();
+            },
+            steps: steps,
+            config: CoolStepperConfig(
+              backText: 'ANTERIOR',
+              nextText: 'SIGUIENTE',
+              finalText: 'ENVIAR',
+              stepText: '',
+              iconColor: mainColor,
+              ofText: 'DE',
+              headerColor: mainColor,
+              titleTextStyle: TextStyle(color: Colors.white, fontSize: 20.0),
+              subtitleTextStyle: TextStyle(color: Colors.white, fontSize: 16.0),
+            ),
+          ),
+          Positioned(
+            child: Text('sss'),
+            top: 30.0,
+            right: 30.0,
+          )
+        ]),
+      ),
+    );
+  }
 
+  List listSteps(context) {
     int cont = 1;
     for (var item in widget.evaluacionInfo.preguntas) {
       // List<Respuesta> repustas = item.respuestas;
       List<Respuesta> respuesta = item.respuestas;
+
+      final List<Map> data = [];
+
+      for (var rs in respuesta) {
+        data.add({'value': rs.delta, 'display': rs.texto});
+      }
 
       preguntasList[item.id] = '';
 
@@ -177,75 +220,81 @@ class __ContentSingleEvaluacionState extends State<_ContentSingleEvaluacion> {
         content: Container(
           child: Column(
             children: <Widget>[
-              for (var rs in respuesta)
-                _buildSelector(
-                    name: rs.texto,
-                    value: rs.delta.toString(),
-                    pregunta: item.id,
-                    context: context),
+              RadioButtonFormField(
+                toggleable: true,
+                padding: EdgeInsets.all(8),
+                context: context,
+                value: 'value',
+                display: 'display',
+                data: data,
+                activeColor: mainColor,
+                autoValidate: true,
+                onSaved: (value) {
+                  setState(() {
+                    preguntasList[item.id] = value.toString();
+                  });
+                },
+                validator: (value) {
+                  if (value == null) {
+                    return 'no pasa';
+                  }
+                  return null;
+                },
+              ),
             ],
           ),
         ),
         validation: () {
+          setState(() {
+            _autoValidate = true;
+          });
+
+          if (!_key.currentState.validate()) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Marca una casilla para continuar'),
+                backgroundColor: mainColor,
+              ),
+            );
+
+            return 'no-pasa';
+          }
+
+          _key.currentState.save();
+
           return null;
         },
       ));
       cont++;
     }
 
-    // print(preguntasList);
-
-    return Container(
-      child: Stack(children: [
-        CoolStepper(
-          showErrorSnackbar: false,
-          onCompleted: () {
-            print('Steps completed!');
-          },
-          steps: steps,
-          config: CoolStepperConfig(
-            backText: 'ANTERIOR',
-            nextText: 'SIGUIENTE',
-            stepText: '',
-            iconColor: mainColor,
-            ofText: 'DE',
-            headerColor: mainColor,
-            titleTextStyle: TextStyle(color: Colors.white, fontSize: 20.0),
-            subtitleTextStyle: TextStyle(color: Colors.white, fontSize: 16.0),
-          ),
-        ),
-        Positioned(
-          child: Text('sss'),
-          top: 30.0,
-          right: 30.0,
-        )
-      ]),
-    );
+    return steps;
   }
 
-  Widget _buildSelector(
-      {BuildContext context,
-      @required String name,
-      @required String value,
-      @required String pregunta}) {
-    final isActive = name == selectedRole;
+  _onSave() {
+    // _key.currentState.save();
+    print(preguntasList);
+    print('pasa');
 
-    return RadioListTile(
-      value: value,
-      activeColor: mainColor,
-      groupValue: selectedRole,
-      onChanged: (v) {
-        _setValue(pregunta, v);
-        setState(() {
-          selectedRole = v;
-        });
-      },
-      title: Text(
-        name,
-        style: TextStyle(
-          color: isActive ? Colors.white : null,
-        ),
+    _key.currentState.save();
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('!DESEA ENVIAR!'),
+        content: const Text('AlertDialog description'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'OK'),
+            child: const Text('OK'),
+          ),
+        ],
       ),
     );
+
+    // print(preguntasList);
   }
 }
